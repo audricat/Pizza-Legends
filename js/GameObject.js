5b77ecce020d664c5efb3efca74d368f4536eac8
S.a <shanenprz@gmail.com>
2021-11-08 14:01:58 +0800

    Update GameObject.js

--- js/GameObject.js
+++ js/GameObject.js
@@ -1,10 +1,11 @@
+import hero from './images/characters/hero.png'
 class GameObject {
     constructor(config) {
         this.x = config.x || 0;
         this.y = config.y || 0;
         this.sprite = new Sprite({
             gameObject: this,
-            src: config.src || "images/characters/hero.png"
+            src: config.src || "hero"
         });
     }
 }
